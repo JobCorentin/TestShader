7c3040e4bc707214d058c9445e4ec6c5a886c31d
Job Corentin <job.corentin@outlook.fr>
2020-09-23 17:57:22 +0200

    Shader graph

---
+++ Shader_Test/Assets/Scenes/NewAnimatorWorkflow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewAnimatorWorkflow : MonoBehaviour
+{
+    Animator animator;
+    private string currentState;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeAnimationState (string newState)
+    {
+        if (currentState == newState) return;
+
+        animator.Play(newState);
+        currentState = newState;
+    }
+}
