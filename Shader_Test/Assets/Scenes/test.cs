2a886f39aaed4a280541e330e7df173b5aeb07ed
Corentin <job.corentin@outlook.fr>
2020-09-23 08:54:07 +0200

    ok

---
+++ Shader_Test/Assets/Scenes/test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public List<GameObject> list;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetFalseList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown("a"))
+        {
+            SetFalseList();
+            list[0].gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown("z"))
+        {
+            SetFalseList();
+            list[1].gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            SetFalseList();
+            list[2].gameObject.SetActive(true);
+        }
+    }
+
+    void SetFalseList()
+    {
+        foreach (GameObject game in list)
+        {
+            game.SetActive(false);
+        }
+    }
+}
