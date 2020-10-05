840ae0e6cbd9587a6a42f6bb5e947bda30064df3
Corentin <job.corentin@outlook.fr>
2020-10-05 09:33:42 +0200

    bravo

---
+++ Shader_Test/Assets/Scene3/Scripts/PostProcess/MaterialManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class MaterialManager : MonoBehaviour
+{
+    public List<Material> pbrMat = new List<Material>();
+    float intensity;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            pbrMat[0].SetColor("_Outline_Color", Color.red);
+        }
+    }
+}
